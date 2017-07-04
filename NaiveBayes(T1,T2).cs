4b235631d5c5873696aaf329f79ecba93c0791ae
S78 <shigeru78@yahoo.com>
2017-07-04 16:00:26 -0500

    Create NaiveBayes(T1,T2).cs

---
+++ NaiveBayes(T1,T2).cs
@@ -0,0 +1,13 @@
+namespace S78
+{
+    public class NaiveBayes<T1, T2>
+    {
+        public void Train(TupleList<T1, T2> data)
+        {
+        }
+
+        public PredictionResult<T1>[] Predict(T2 value)
+        {
+        }
+    }
+}
